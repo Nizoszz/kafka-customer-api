27a43e0d322d96c93054a66646e726122c6cf75b
Andrew da Silva <andrewairamdasilva@gmail.com>
2025-05-05 10:19:31 -0300

    feat(init): scaffolding project

---
+++ src/test/java/com/n1z0/customer_app/CustomerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n1z0.customer_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
